9b99e695666d145965f466a1831db5f97798ade5
Andre Rabold <andre.rabold@gmail.com>
2025-03-30 06:47:28 -0700

    ci: optimize dependency installation in CI workflow
    Updates the `npm ci` command in the CI workflow to use `--only=production` and `--ignore-scripts`. This optimizes the installation process by skipping development dependencies and lifecycle scripts, potentially speeding up the workflow.

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,4 +2,7 @@
 /** @type {import('@commitlint/types').UserConfig} */
 export default {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-max-line-length": [0, "always", 100],
+  },
 };
