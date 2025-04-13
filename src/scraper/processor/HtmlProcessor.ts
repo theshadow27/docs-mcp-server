6c76509b80dd48a5a21923b54f985c456c19d46a
Andre Rabold <andre.rabold@gmail.com>
2025-04-13 16:29:00 -0700

    fix: remove overly aggressive html filtering
    this commit:
    - removes figure and sup tags from html filtering
    - keeps alert and role=alert selectors commented out
    - adds explanatory comments about alerts being used for important content
    
    fixes #36

--- src/scraper/processor/HtmlProcessor.ts
+++ src/scraper/processor/HtmlProcessor.ts
@@ -66,18 +66,16 @@ export class HtmlProcessor implements ContentProcessor {
     ".signup-form",
     ".tooltip",
     ".dropdown-menu",
-    ".alert",
+    // ".alert", // Known issue: Some pages use alerts for important content
     ".breadcrumb",
     ".pagination",
-    '[role="alert"]',
+    // '[role="alert"]', // Known issue: Some pages use alerts for important content
     '[role="banner"]',
     '[role="dialog"]',
     '[role="alertdialog"]',
     '[role="region"][aria-label*="skip" i]',
     '[aria-modal="true"]',
     ".noprint",
-    "figure",
-    "sup",
   ];
 
   constructor(options?: HtmlProcessOptions) {
